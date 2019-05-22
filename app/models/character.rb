8c7b4128fc9be9e64de974f2af8d3a1154da76cf
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-05-22 12:41:06 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,6 @@
 class Character < ActiveRecord::Base
   belongs_to :show
-  belongs_to :actor, tro
+  belongs_to :actor, through: :show
   
   def say_that_thing_you_say
     "#{name} always says: #{catchphrase}"
